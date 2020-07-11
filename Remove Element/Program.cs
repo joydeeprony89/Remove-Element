4ccb3d9bc9f59de963ef226b71138ebb0fcdf5e1
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-11 20:55:24 +0200

    Add project files.

---
+++ Remove Element/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int counter = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val) 
+                {
+                    nums[counter] = nums[i];
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
